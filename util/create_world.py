d01c1dbd9619c8cb9b77345a1f96bcc2cd476e56
Arron M <marshall.arron@gmail.com>
2019-09-25 15:51:23 -0700

    Transfer vars to environment

--- util/create_world.py
+++ util/create_world.py
@@ -1,16 +1,15 @@
 from django.contrib.auth.models import User
 from adventure.models import Player, Room
 from util.arronm_generator import World
-
+from decouple import config
 
 Room.objects.all().delete()
 
 w = World()
-num_rooms = 100
-width = 20
-height = 20
-seed = "ceDCxJTZ370qqSq5"
-# seed = None
+num_rooms = config("GRID_ROOMS", default=100, cast=int)
+width = config("GRID_WIDTH", default=20, cast=int)
+height = config("GRID_HEIGHT", default=20, cast=int)
+seed = config("GRID_SEED", default=None)
 spawn = w.generate_rooms(width, height, num_rooms, seed)
 w.print_rooms()
 
